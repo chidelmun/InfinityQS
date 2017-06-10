0d54a76223111488e7e09cca722dfac5867341f7
Delveri Munang <chickdelveri@gmail.com>
2017-06-10 17:20:44 -0400

    Added screenshot of first build

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("InfinityQS v1.0.9 Beta");
-        primaryStage.setScene(new Scene(root, 800, 800));
+        primaryStage.setScene(new Scene(root, 1400, 800));
         primaryStage.show();
     }
 
