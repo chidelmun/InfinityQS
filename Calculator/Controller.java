cc03643b95b1215552c80ccf87a785331b0d50e7
Delveri Munang <chickdelveri@gmail.com>
2017-06-12 13:44:33 -0400

    Added getter methods to buttons

--- Calculator/Controller.java
+++ Calculator/Controller.java
@@ -1,6 +1,8 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.event.*;
 
 public class Controller {
 
@@ -11,13 +13,19 @@ public class Controller {
     private Button perionButton;
 
     @FXML
+    private Label result;
+
+    @FXML
     void getPeriodValue(ActionEvent event) {
     	System.out.println("Period Button Clicked");
+    	result.setText(zerobutton.getText());
     }
 
     @FXML
     void getZeroButtonValue(ActionEvent event) {
     	System.out.println("Zero Button Clicked");
+    	System.out.println(result.getText());
+    	result.setText("Clicked");
     }
 
 }
