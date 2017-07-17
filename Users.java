5496845c182546b62f3d2eb3bed6f7feb05a430e
Delveri Munang <chickdelveri@gmail.com>
2017-07-16 22:17:53 -0400

    Added UI components

--- Users.java
+++ Users.java
@@ -20,15 +20,23 @@ public class Users  implements Initializable{
         ArrayList<String> scripts = new ArrayList<String>();
         try{  
             Class.forName("com.mysql.jdbc.Driver");  
-            Connection con=DriverManager.getConnection(  
+            Connection con = DriverManager.getConnection(  
             "jdbc:mysql://localhost:3306/mailapp","root","root");  
             //here sonoo is database name, root is username and password  
             Statement stmt=con.createStatement();  
-            ResultSet rs=stmt.executeQuery("select * from users");  
-            while(rs.next())  
-            System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)); 
-            scripts.add(rs.getString(3));
+            ResultSet rs=stmt.executeQuery("select * from users"); 
+            ArrayList<String> output = new ArrayList<String>();
+              while (rs.next()) {
+                output.add("Read from DB: " + rs.getString("display_name"));
+              } 
+            // while(rs.next())  
+            // System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3)); 
+            // scripts.add(rs.getString(3));
             con.close();  
+
+            for(int i = 0; i< output.size() ; ++i){
+                System.out.println(output);
+            }
             }catch(Exception e){
                 System.out.println(e);
             }
